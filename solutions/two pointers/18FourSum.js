2d828b9bfd55b37ded4f6b5d12de8a4aa2618927
David Besnovatiy <davidbesnovatiy@gmail.com>
2025-04-21 13:46:05 +0300

    daily with some sum problems

---
+++ solutions/two pointers/18FourSum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function (nums, target) {
+    let res = [];
+    nums.sort((a, b) => a - b);
+    const n = nums.length;
+
+    for (let i = 0; i < n - 3; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        for (let j = i + 1; j < n - 2; j++) {
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+            let l = j + 1, r = n - 1;
+            while (l < r) {
+                const sum = nums[i] + nums[j] + nums[l] + nums[r];
+                if (sum === target) {
+                    res.push([nums[i], nums[j], nums[l], nums[r]]);
+                    while (l < r && nums[l] === nums[l + 1]) l++;
+                    while (l < r && nums[r] === nums[r - 1]) r--;
+                    l++;
+                    r--;
+                } else if (sum < target) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+    }
+
+    return res;
+};
