c483452b2f6f5bbc19c26b4890b7878068ad01d1
David Besnovatiy <davidbesnovatiy@gmail.com>
2025-04-23 14:58:42 +0300

    daily

---
+++ solutions/daily/1399CountLargestGroup.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countLargestGroup = function (n) {
+    const map = new Map();
+    let maxSize = 0;
+
+    for (let i = 1; i <= n; i++) {
+        const sum = i.toString().split('').reduce((acc, d) => acc + Number(d), 0);
+        const newCount = (map.get(sum) || 0) + 1;
+        map.set(sum, newCount);
+        maxSize = Math.max(maxSize, newCount);
+    }
+
+    let result = 0;
+    for (let count of map.values()) {
+        if (count === maxSize) result++;
+    }
+    return result;
+};
